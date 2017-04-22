25ddc67115baf408b118075b78a779a419370e5e
Felipe Guimarães <Felipe Guimares>
2017-04-22 03:48:55 -0300

    Implemented basic plataform mechanics, working on the plot of the game too.

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+	private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = this.transform.parent.gameObject.GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "World" || other.gameObject.tag == "Platform") {
+			player.GotFloor();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		player.ExitFloor ();
+	}
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	private bool onFloor;
+	private bool canJump;
+	public float hSpeed = 2f;
+	private float vSpeed = 0f;
+
+	public float jumpSpeed = 10f;
+	public float grav = -10f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +19,40 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		doHMovment ();
+		doVMovment ();
+		if(Input.GetKeyDown(KeyCode.W))
+			Jump(jumpSpeed);
+	}
+
+	//Simple horizontal moviment function
+	public void doHMovment(){ 
+		transform.position = new Vector3 (transform.position.x + hSpeed * Time.deltaTime * Input.GetAxisRaw ("Horizontal"),
+			transform.position.y, transform.position.z); 
+	}
+
+	//Simple vartical movment function
+	public void doVMovment(){
+		transform.position = new Vector3 (transform.position.x,
+			transform.position.y + vSpeed * Time.deltaTime, transform.position.z);
+		if(!onFloor)
+			vSpeed += Time.deltaTime * grav;
+	}
+
+	public void Jump(float _jumpSpeed){
+		if (canJump) {
+			vSpeed = _jumpSpeed;
+			onFloor = false;
+			canJump = false;
+		}
+	}
+	public void GotFloor(){
+		vSpeed = 0f;
+		onFloor = true;
+		canJump = true;
+	}
+
+	public void ExitFloor(){
+		onFloor = false;
 	}
 }
